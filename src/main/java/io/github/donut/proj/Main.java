03becc616b6bf967578da6020a68c2ab6d3c9a36
Kord Boniadi <kordbon1@gmail.com>
2021-04-14 19:11:55 -0700

    removed debug code

--- src/main/java/io/github/donut/proj/Main.java
+++ src/main/java/io/github/donut/proj/Main.java
@@ -25,7 +25,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println(GsonWrapper.toJson(MoveData.of("1", 1, 2, "X")));
         new AppController(stage).startApp();
     }
 
