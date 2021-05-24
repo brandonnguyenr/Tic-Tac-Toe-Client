f099e712ce5fca9257679454ef44836f5207cccb
joeyCampbelll <joey.m.campbell@gmail.com>
2021-05-23 20:09:37 -0700

    switching to kords branch

--- src/main/java/io/github/donut/proj/callbacks/HistoryCallback.java
+++ src/main/java/io/github/donut/proj/callbacks/HistoryCallback.java
@@ -1,4 +1,40 @@
 package io.github.donut.proj.callbacks;
 
-public class HistoryCallback {
+import io.github.API.ISubscribeCallback;
+import io.github.API.MessagingAPI;
+import io.github.API.messagedata.MsgResultAPI;
+import io.github.API.messagedata.MsgStatus;
+import io.github.API.utils.GsonWrapper;
+import io.github.coreutils.proj.messages.Channels;
+import io.github.coreutils.proj.messages.RoomResponse;
+import io.github.donut.proj.listener.ISubject;
+
+import java.util.ArrayList;
+
+public class HistoryCallback implements ISubscribeCallback, ISubject {
+
+    private final Runnable resolved;
+    private final Runnable rejected;
+
+    public HistoryCallback(Runnable resolved, Runnable rejected) {
+        this.resolved = resolved;
+        this.rejected = rejected;
+    }
+
+    @Override
+    public void status(MessagingAPI messagingAPI, MsgStatus msgStatus) {
+
+    }
+
+    @Override
+    public void resolved(MessagingAPI messagingAPI, MsgResultAPI msgResultAPI) {
+        if (msgResultAPI.getChannel().equals(Channels.PRIVATE + messagingAPI.getUuid())) {
+            ArrayList<RoomResponse> response = GsonWrapper.fromJson(msgResultAPI.getMessage(), RoomResponse.class);
+        }
+    }
+
+    @Override
+    public void rejected(Exception e) {
+
+    }
 }
