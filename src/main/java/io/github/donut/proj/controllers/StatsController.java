ae5f5ac6586fc918110732b6830eb2bf2c825e26
utsavparajuli <73618451+utsavparajuli@users.noreply.github.com>
2021-05-27 23:21:15 -0700

    making changes to stats page

--- src/main/java/io/github/donut/proj/controllers/StatsController.java
+++ src/main/java/io/github/donut/proj/controllers/StatsController.java
@@ -1,14 +1,24 @@
 package io.github.donut.proj.controllers;
 
+import io.github.coreutils.proj.messages.Channels;
+import io.github.coreutils.proj.messages.RoomResponse;
+import io.github.donut.proj.callbacks.GlobalAPIManager;
+import io.github.donut.proj.callbacks.RoomHistoryCallback;
 import io.github.donut.proj.listener.ISubject;
 import io.github.donut.proj.model.SceneName;
 import io.github.donut.sounds.EventSounds;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -22,6 +32,11 @@ public class StatsController extends AbstractController implements ISubject {
     private Label ties;
     @FXML
     private Label losses;
+    @FXML
+    private Label winPercentageTitle;
+
+    @FXML
+    private Label total;
 
     @FXML
     private Label wins1;
@@ -31,16 +46,22 @@ public class StatsController extends AbstractController implements ISubject {
     private Label losses1;
 
     @FXML
+    private Label winPercentage;
+
+    @FXML
     private ImageView backButton;
+
+    private List<RoomResponse> rooms;
 
     /**
      * Initializes the stats page
      */
     @FXML
     public void initialize() {
-        wins.setText  ("20");
-        ties.setText  ("1");
-        losses.setText("0");
+
+        GlobalAPIManager.getInstance().swapListener(new RoomHistoryCallback(this::setRoomsListAsync),
+                Channels.REQUEST + Channels.GET_ROOMS_DATA.toString(),
+                Channels.PRIVATE + GlobalAPIManager.getInstance().getApi().getUuid());
 
         /*========================Action Events START=========================*/
         backButton.setOnMouseClicked(this::onBackButtonClick);
@@ -49,6 +70,49 @@ public class StatsController extends AbstractController implements ISubject {
         /*========================Action Events END===========================*/
     }
 
+    public void setRoomsListAsync(List<RoomResponse> rooms) {
+        Platform.runLater(() -> {
+            getStats(rooms);
+        });
+    }
+
+    public void getStats(List<RoomResponse> rooms) {
+        this.rooms = rooms;
+
+        int winCount = 0;
+        int lossCount = 0;
+        int tieCount = 0;
+
+        for(RoomResponse room : rooms) {
+            switch(room.getResult()) {
+                case "WIN":
+                    winCount++;
+                    break;
+                case "TIE":
+                    tieCount++;
+                    break;
+                default:
+                    lossCount++;
+            }
+        }
+
+        wins.setText("" + winCount);
+        ties.setText("" + tieCount);
+        losses.setText("" + lossCount);
+
+        int totalGames = winCount + tieCount + lossCount;
+
+        System.out.println(totalGames);
+        total.setText("" + totalGames);
+
+        if (totalGames == 0) {
+            winPercentage.setText("N/A");
+        } else {
+            double percentage = (winCount + (tieCount * 0.5 )) / totalGames;
+            System.out.println(percentage);
+            winPercentage.setText("" + String.format("%.2f", percentage));
+        }
+    }
     /**
      * Event handler for back button
      *
